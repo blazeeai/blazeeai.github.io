ffb5af3b6cf2de676f009bb75b0e5b3558032307
blazeeai <blazeappai@gmail.com>
2025-02-02 19:26:43 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4111372603"
+    serviceWorkerVersion: "1566305012"
   }
 });
